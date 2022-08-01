b6af3ecc65ddaeaa9e950c2f304793ccda3865ba
Milan Bojic <milan.bojic@zuehlke.com>
2022-08-01 14:28:54 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Milan Bojic on 1.8.22..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
