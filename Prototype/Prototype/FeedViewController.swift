5f8b7dc9d0c7f91c3baac268af106f1a0137bf27
Milan Bojic <milan.bojic@zuehlke.com>
2022-11-21 18:00:23 +0100

    Add tableView data source and title

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Milan Bojic on 21.11.22..
+//
+
+import UIKit
+
+class FeedViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    }
+}
