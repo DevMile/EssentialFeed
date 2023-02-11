5d008e9bc6fb1cc539e15eb87c2eff441c29bd7b
Milan Bojic <milan.bojic@zuehlke.com>
2023-02-11 16:05:44 +0100

    Write adapter pattern example in `FeedUIComposer` to clarify intent

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -22,4 +22,14 @@ public final class FeedUIComposer {
         return feedController
     }
     
+    // Adapter pattern - mapping [FeedImage] into FeedImageController - we can write also like this:
+    // refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+    
+//    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+//        return { [weak controller] feed in
+//            controller?.tableModel = feed.map { model in
+//                FeedImageCellController(model: model, imageLoader: loader)
+//            }
+//        }
+//    }
 }
