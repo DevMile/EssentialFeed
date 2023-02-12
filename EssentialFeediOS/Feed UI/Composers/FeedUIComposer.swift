143e040927abd1870bca7e996efd29cbca8a5d9f
Milan Bojic <milan.bojic@zuehlke.com>
2023-02-12 11:30:26 +0100

    Move `FeedLoader` loading state management to `FeedViewModel`. Now, the `FeedRefreshViewController` acts as a binder between the `View` and the `ViewModel`

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,9 +12,10 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
+        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
+        let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
         let feedController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = { [weak feedController] feed in
+        feedViewModel.onFeedLoad = { [weak feedController] feed in
             feedController?.tableModel = feed.map { model in
                 FeedImageCellController(model: model, imageLoader: imageLoader)
             }
@@ -23,7 +24,7 @@ public final class FeedUIComposer {
     }
     
     // Adapter pattern - mapping [FeedImage] into FeedImageController - we can write also like this:
-    // refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+    // feedViewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
     
 //    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
 //        return { [weak controller] feed in
