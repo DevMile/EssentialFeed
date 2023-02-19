bd8b3d9d754824af4a95b16801edfd2109140147
Milan Bojic <milan.bojic@zuehlke.com>
2023-02-19 15:11:03 +0100

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Milan Bojic on 19.2.23..
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Milan Bojic on 19.2.23..
+//
+
+import Foundation
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
