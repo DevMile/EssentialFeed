e34982f37cd2e29cbbf103d5b45fb284809a23a0
Milan Bojic <milan.bojic@zuehlke.com>
2023-08-22 22:35:39 +0200

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Milan Bojic on 22.8.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
