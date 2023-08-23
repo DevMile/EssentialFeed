8a6dda1285979b4007f6903be7e305032728b3da
Milan Bojic <milan.bojic@zuehlke.com>
2023-08-23 19:33:46 +0200

    Move FeedLoaderWithFallbackComposite to production

---
+++ EssentialApp/EssentialApp/FeedLoaderWithCallbackComposite.swift
@@ -0,0 +1,30 @@
+//
+//  FeedLoaderWithCallbackComposite.swift
+//  EssentialApp
+//
+//  Created by Milan Bojic on 23.8.23..
+//
+
+import EssentialFeed
+
+public class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
+
+    public init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
+    }
+}
